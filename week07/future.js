ecdc7a8d94699af7b0156286dc1c8180dd91fbf8
Sunshine Calderon <86927739+Suncalde@users.noreply.github.com>
2021-10-11 17:33:56 -0500

    styled fortune

--- week07/future.js
+++ week07/future.js
@@ -1,9 +1,9 @@
 var job = ["Student","Softwear Engineer","Chef","Trophery Wife"];
-var city = ["Dallas","Oaxaca","Berlin","Seattle","Tokyo"];
-var spouse = ["Wasim","Johnny Depp","Ryan Reynolds","Gong Yoo","Kit Harington"];
+var city = ["Dallas","Oaxaca","Berlin","Seattle"];
+var spouse = ["Wasim","Ryan Reynolds","Gong Yoo","Kit Harington"];
 var numKids = [0, 3, 4, 6, 12];
 
-var xx = Math.floor(Math.random() * 5)
+var xx = Math.floor(Math.random() * 4)
 
 function fortune(job,city,spouse,numKids) {
 
